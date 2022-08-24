d0d5d64d11e15c7d8ba4f296d4784818d64e97fe
Jared <jbeckersalax9@aol.com>
2022-08-24 16:21:06 -0400

    render sign up errors

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -25,18 +25,20 @@ function SignUpForm({ setSignUpClick }){
     }
 
     function handleSignUpErrors(userArg){
-        console.log(userArg.errors)
         if (userArg.errors){
-            setSignUpErrors(userArg.errors)
+            setSignUpErrors(userArg.errors.join(" "))
         } else {
             setSignUpErrors(null)
+            setSignUpClick(false)
         }
     }
 
+    
+
     return(
         <div>
+            <button className="SignUp-backToLoginButton" onClick={() => setSignUpClick(false)}> X </button>
             <h1 className="SignUp-h1"> Sign Up </h1>
-            { signUpErrors ? signUpErrors.forEach((errorMessage) => <h3 className="SignUp-h3">{errorMessage}</h3>) : null }
             <form onSubmit={(e) => handleSignUpSubmit(e)} >
                 <input 
                     className="SignUpForm-input"
@@ -44,23 +46,24 @@ function SignUpForm({ setSignUpClick }){
                     type = "text"
                     value = { username }
                     placeholder="Username..."
-                />
+                    />
                 <input 
                     className="SignUpForm-input"
                     onChange={ (e) => setPassword(e.target.value) }
                     type = "password"
                     value = { password } 
                     placeholder="Password..."
-                />
+                    />
                 <input 
                     className="SignUpForm-input"
                     onChange={ (e) => setPasswordConfirmation(e.target.value) }
                     type = "password"
                     value = {passwordConfirmation}
                     placeholder="Confirm Password..."
-                />
+                    />
                 <button className="SignUp-submitButton" type = "submit" > Create an Account </button>
             </form>
+            <h3 className="SignUp-h3"> {signUpErrors} </h3>
         </div>
     )
 }
