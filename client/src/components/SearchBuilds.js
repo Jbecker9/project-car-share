b8b93b36ccb19e9865486adf0dc011f4a5c3f48f
Jared <jbeckersalax9@aol.com>
2022-08-26 11:54:15 -0400

    search build simplified to search by company

--- client/src/components/SearchBuilds.js
+++ client/src/components/SearchBuilds.js
@@ -1,61 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/SearchBuilds.css"
 import CompanySearch from "./CompanySearch";
-import NameSearch from "./NameSearch";
 
 
 function SearchBuilds({ makes }){
-    const [isNameFilterActive, setisNameFilterActive] = useState(false)
-    const [isCompanyFilterActive, setisCompanyFilterActive] = useState(false)
-    const [isEngineFilterActive, setisEngineFilterActive] = useState(false)
-
-    function nameFilterButtonActive(e){
-        e.preventDefault()
-        setisCompanyFilterActive(false)
-        setisEngineFilterActive(false)
-        setisNameFilterActive(true)
-    }
-    
-    function companyFilterButtonActive(e){
-        e.preventDefault()
-        setisEngineFilterActive(false)
-        setisNameFilterActive(false)
-        setisCompanyFilterActive(true)
-    }
-
-    function engineFilterButtonActive(e){
-        e.preventDefault()
-        setisEngineFilterActive(true)
-        setisNameFilterActive(false)
-        setisCompanyFilterActive(false)
-    }
 
     return(
         <div>
-            <div className="SearchBuilds-filterBar">
-                <h3 className="SearchBuilds-filterTitle">Filter By:</h3>
-                <button 
-                    className={ isNameFilterActive ? "SearchBuilds-filterButtonActive" : "SearchBuilds-filterButton"} 
-                    onClick={(e) => nameFilterButtonActive(e)}
-                >
-                        Name
-                </button>
-                <button 
-                    className={ isCompanyFilterActive ? "SearchBuilds-filterButtonActive" : "SearchBuilds-filterButton"} 
-                    onClick={(e) => companyFilterButtonActive(e)}
-                >
-                        Company
-                </button>
-                <button 
-                    className={ isEngineFilterActive ? "SearchBuilds-filterButtonActive" : "SearchBuilds-filterButton"} 
-                    onClick={(e) => engineFilterButtonActive(e)}
-                >
-                    Engine
-                </button>
-            </div>
             <div className="SearchBuilds-searchDiv">
-            { isNameFilterActive ? <NameSearch /> : null }
-            { isCompanyFilterActive ? <CompanySearch makes={makes} /> : null }
+            <CompanySearch makes={makes} />
             </div>
         </div>
     )
